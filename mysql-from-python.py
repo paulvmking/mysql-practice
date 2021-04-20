38c8963e5bf280624b7615d3a2404ccde4753d7f
paulvmking <paulvmking@gmail.com>
2021-04-20 17:46:09 +0000

    added text to create new table in database

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -1,4 +1,5 @@
 import os 
+import datetime
 import pymysql
 
 username = os.getenv('VS_USER')
@@ -11,10 +12,8 @@ connection = pymysql.connect(host='localhost',
 try:
     # Run a query
     with connection.cursor(pymysql.cursors.DictCursor) as cursor:
-        sql = "SELECT * FROM Genre;"
-        cursor.execute(sql)
-        for row in cursor:
-            print(row)
+        cursor.execute("""CREATE TABLE IF NOT EXISTS
+                          Friends(name char(20), age int, DOB datetime);""")
 finally:
     # Close the connection, regardless of whether or not the above was
     # successful
