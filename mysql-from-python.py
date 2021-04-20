da3f17c4c84369485de41e1d9f7ccd86c96d9747
paulvmking <paulvmking@gmail.com>
2021-04-20 17:16:21 +0000

    connected python to sql and tested pulling data

---
+++ mysql-from-python.py
@@ -0,0 +1,21 @@
+import os 
+import pymysql
+
+username = os.getenv('VS_USER')
+
+connection = pymysql.connect(host='localhost',
+                             user=username,
+                             password='',
+                             db='Chinook')
+
+try:
+    # Run a query
+    with connection.cursor() as cursor:
+        sql = "SELECT * FROM Artist;"
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        print(result)
+finally:
+    # Close the connection, regardless of whether or not the above was 
+    # successful
+    connection.close()
